0169b62c3852337e984502ab5c22f631a4f2073d
Virginia <virginialash1@gmail.com>
2019-02-08 14:39:52 -0500

    trying to fix deployement issue

---
+++ script.js
@@ -0,0 +1,112 @@
+//...............variables.................
+const nextQuestionButton = document.querySelector('.nextQ');
+const currentQuestion = document.querySelector('.questions');
+const option_1 = document.querySelector('.option1');
+const option_2 = document.querySelector('.option2');
+let playerScore = document.querySelector('.Score');
+let currentScore = 000;
+
+
+class Question {
+    constructor(questionString,answerChoices,correctAnswerIndex){
+        this.questionString= questionString;
+        this.answerChoices= answerChoices;
+        this.correctAnswerIndex = correctAnswerIndex;
+}
+}
+//..............questions array .......
+const questionsArray = [
+    new Question("Which emperor attempted to make his horse, Incitatus, a Consul (the highest elected office of the Roman Republic)?",["Caligula","Nero"],0),
+    new Question("What substance did ancient Roman woman rub on their skin to stay lookin' fresh?",["Dead skin cells from Gladiators","Goat Poop"],0),
+    new Question("What was the max amount of times a father could sell their sons into slavery?",["Two Times","Three Times"],1),
+    new Question("'Emperor Heliogabalus loved collecting what items by the ton?",["Cobwebs","Chicken feathers"],0),
+    new Question("Emperor Nero played what instrument while watching Rome burn?",["Bugle","Lyre"],1)
+]
+
+
+
+
+let questionIndex = 0;
+
+let newQuestion = questionsArray[questionIndex];
+currentQuestion.innerHTML = newQuestion;
+currentQuestion.innerText = newQuestion.questionString;
+option_1.innerText = newQuestion.answerChoices[0];
+option_2.innerText= newQuestion.answerChoices[1];
+
+
+nextQuestionButton.addEventListener('click', function() {
+    console.log(questionsArray.length)
+    console.log(questionIndex)
+    questionIndex += 1;
+    if (questionIndex < questionsArray.length) {
+        let newQuestion = questionsArray[questionIndex];
+        currentQuestion.innerHTML = " ";
+        option_1.style.backgroundColor = "white";
+        option_2.style.backgroundColor ="white";
+        currentQuestion.innerHTML = newQuestion;
+        currentQuestion.innerText = newQuestion.questionString;
+        option_1.innerText = newQuestion.answerChoices[0];
+        option_2.innerText= newQuestion.answerChoices[1];
+    } else {
+        // document.querySelector('.results').style.display = 'unset';
+        document.querySelector('.gameboard').style.display = 'none';
+        if (currentScore == 500) {
+            document.querySelector('.results').innerText = "ZEUS WOULD BE PROUD!";
+        } else if (currentScore == 400) {
+            document.querySelector('.results').innerText = "THE EMPEROR IS IMPRESSED";
+        } else if (currentScore == 300) {
+            document.querySelector('.results').innerText = "CESAR FROWNS AT YOU";
+        }else if (currentScore == 200) {
+            document.querySelector('.results').innerText = "CESAR FROWNS AT YOU";
+        }
+        else if (currentScore == 100) {
+            document.querySelector('.results').innerText = "CESAR FROWNS AT YOU";
+        }  else if (currentScore == 000) {
+            document.querySelector('.results').innerText = "YOU'RE LITERALLY SO BAD";
+        } 
+        document.querySelector('.results').style.visibility = "visible";
+     }
+
+    });
+
+
+
+var question0 =  new Question("Which emperor attempted to make his horse, Incitatus, a Consul (the highest elected office of the Roman Republic)?",["Caligula","Nero"], 0);
+
+
+
+    option_1.setAttribute('data-index', 0);
+    option_2.setAttribute('data-index', 1);
+
+
+    const optionsParent = document.querySelector('.options')
+    
+    optionsParent.addEventListener('click', function(evt) {
+        const target= evt.target
+        
+        const optionIndex = Number(target.getAttribute('data-index'))
+
+        console.log(questionsArray[questionIndex])
+
+        if (target.classList.contains('option')) {
+            if (questionsArray[questionIndex].correctAnswerIndex === optionIndex) {
+                console.log('that is the correct answer!!!!')
+                target.style.backgroundColor ='green'
+                currentScore += 100,
+                console.log(currentScore)
+                console.log(playerScore)
+                playerScore.innerText = currentScore
+            } else {
+                console.log('sorry, that is incorrect')
+                target.style.backgroundColor ='red'
+            }
+        }
+        
+
+    })
+
+
+
+  //.............. events.............
+// https://codepen.io/jkohlin/pen/MQPBYJ?editors=0010 - help referenced 
